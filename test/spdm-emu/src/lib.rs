5fff8b11554dafd921b1df4d8a415ab72faab08e
Yang, Longlong <longlong.yang@intel.com>
2023-08-06 20:05:20 -0400

    clean up stack size definition.
    fix #23
    
    Signed-off-by: Yang, Longlong <longlong.yang@intel.com>

--- test/spdm-emu/src/lib.rs
+++ test/spdm-emu/src/lib.rs
@@ -12,19 +12,16 @@ pub mod spdm_emu;
 
 use std::mem::size_of;
 
-use mctp_transport::MctpTransportEncap;
-use pcidoe_transport::PciDoeTransportEncap;
-use socket_io_transport::SocketIoTransport;
+use mctp_transport::{MctpTransportEncap, MCTP_TRANSPORT_STACK_SIZE};
+use pcidoe_transport::{PciDoeTransportEncap, PCIDOE_TRANSPORT_STACK_SIZE};
+use socket_io_transport::DEVICE_IO_STACK_SIZE;
 use spdmlib::{
-    config::{
-        MAX_SPDM_CERT_CHAIN_DATA_SIZE, MAX_SPDM_MEASUREMENT_RECORD_SIZE,
-        MAX_SPDM_MEASUREMENT_VALUE_LEN, MAX_SPDM_MSG_SIZE, MAX_SPDM_PSK_CONTEXT_SIZE,
-        MAX_SPDM_PSK_HINT_SIZE, RECEIVER_BUFFER_SIZE, SENDER_BUFFER_SIZE,
-    },
+    config::{RECEIVER_BUFFER_SIZE, SENDER_BUFFER_SIZE},
     protocol::{
         SpdmCertChainBuffer, SpdmCertChainData, SpdmMeasurementRecordStructure,
         SPDM_MAX_SLOT_NUMBER,
     },
+    SPDM_STACK_SIZE,
 };
 use std::net::TcpStream;
 
@@ -37,22 +34,9 @@ pub const fn MAX(a: usize, b: usize) -> usize {
     }
 }
 
-const TRANSPORT_SIZE: usize = MAX(
-    size_of::<PciDoeTransportEncap>(),
-    size_of::<MctpTransportEncap>(),
-);
-const DEVICE_IO_SIZE: usize = size_of::<SocketIoTransport>();
-const SPDMLIB_SIZE: usize = size_of::<spdmlib::common::SpdmContext>() +
-                            size_of::<SpdmCertChainData>() * (SPDM_MAX_SLOT_NUMBER + 1) + size_of::<SpdmCertChainBuffer>() * SPDM_MAX_SLOT_NUMBER + // SpdmProvisionInfo
-                            size_of::<SpdmCertChainData>() * (SPDM_MAX_SLOT_NUMBER + 1) + // SpdmPeerInfo
-                            (MAX_SPDM_MSG_SIZE + SENDER_BUFFER_SIZE + RECEIVER_BUFFER_SIZE) * 5 + // send/receive + encode/decode
-                            MAX_SPDM_CERT_CHAIN_DATA_SIZE * 8 + // worst case: 8 slots
-                            (MAX_SPDM_MEASUREMENT_RECORD_SIZE + MAX_SPDM_MEASUREMENT_VALUE_LEN) * 255 + // worst case: 255 index
-                            MAX_SPDM_PSK_CONTEXT_SIZE + // for PSK
-                            MAX_SPDM_PSK_HINT_SIZE + // for PSK
-                            size_of::<usize>() * 256; // for general stack case
+const TRANSPORT_STACK_SIZE: usize = MAX(PCIDOE_TRANSPORT_STACK_SIZE, MCTP_TRANSPORT_STACK_SIZE);
 
-const EMU_FUNCTION_STACK: usize = SENDER_BUFFER_SIZE
+const EMU_FUNCTION_STACK_SIZE: usize = SENDER_BUFFER_SIZE
     + RECEIVER_BUFFER_SIZE
     + size_of::<TcpStream>()
     + size_of::<PciDoeTransportEncap>()
@@ -63,4 +47,4 @@ const EMU_FUNCTION_STACK: usize = SENDER_BUFFER_SIZE
     + size_of::<usize>() * 256; // for general stack case
 
 pub const EMU_STACK_SIZE: usize =
-    TRANSPORT_SIZE + DEVICE_IO_SIZE + SPDMLIB_SIZE + EMU_FUNCTION_STACK;
+    TRANSPORT_STACK_SIZE + DEVICE_IO_STACK_SIZE + SPDM_STACK_SIZE + EMU_FUNCTION_STACK_SIZE;

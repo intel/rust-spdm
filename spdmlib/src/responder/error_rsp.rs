b2262986b34fbddec9e252266d12b354f91913a9
Yang, Longlong <longlong.yang@intel.com>
2023-08-30 21:58:05 -0400

    self.common.negotiate_info.spdm_version_sel
    fix #62
    
    Signed-off-by: Yang, Longlong <longlong.yang@intel.com>

--- spdmlib/src/responder/error_rsp.rs
+++ spdmlib/src/responder/error_rsp.rs
@@ -5,6 +5,7 @@
 use crate::common::SpdmCodec;
 use crate::error::SpdmResult;
 use crate::message::*;
+use crate::protocol::SpdmVersion;
 use crate::responder::*;
 
 impl ResponderContext {
@@ -16,7 +17,11 @@ impl ResponderContext {
     ) {
         let error = SpdmMessage {
             header: SpdmMessageHeader {
-                version: self.common.negotiate_info.spdm_version_sel,
+                version: if self.common.negotiate_info.spdm_version_sel.get_u8() == 0 {
+                    SpdmVersion::SpdmVersion10
+                } else {
+                    self.common.negotiate_info.spdm_version_sel
+                },
                 request_response_code: SpdmRequestResponseCode::SpdmResponseError,
             },
             payload: SpdmMessagePayload::SpdmErrorResponse(SpdmErrorResponsePayload {
